6028229f06c32153bde1f183d52d9ca254db27a3
Tarun <tarun.agrawal@spit.ac.in>
2022-10-24 12:40:13 +0530

    deploy commit

--- index.js
+++ index.js
@@ -51,10 +51,7 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true
-}));
+app.use(cors());
 
 
 app.get('/', (req, res) => {
